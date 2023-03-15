c26978884873a282304d23c02f4d4a34231caa18
JuanCarlos <jcge97@gmail.com>
2023-03-15 14:00:02 +0000

    Cambio de búsqueda

--- code/sraping.R
+++ code/sraping.R
@@ -16,8 +16,8 @@ busqueda %>% glimpse()
 x <- entrez_fetch(db="pubmed", id=busqueda, rettype = "abstract")
 
 anio <- 1950:2022
-mus_musculus <- glue("mice genome AND {anio}[PDAT]")
-human_genome <- glue("human genome AND {anio}[PDAT]")
+dna <- glue("dna sequencing AND {anio}[PDAT]")
+rna <- glue("RNA-seq AND {anio}[PDAT]")
 
 human_genome_search <- tibble(
   anio=anio,
@@ -25,14 +25,10 @@ human_genome_search <- tibble(
   mus_musculus = mus_musculus
 ) %>%
   mutate(
-    mice_pubmed = map_dbl(mus_musculus, 
-                          ~entrez_search(db="pubmed", term=.x)$count),
-    human_pubmed = map_dbl(human_genome, 
-                           ~entrez_search(db="pubmed", term=.x)$count),
-    # mice_genome = map_dbl(mus_musculus, 
-    #                       ~entrez_search(db="sra", term=.x)$count),
-    # human_genome2 = map_dbl(human_genome, 
-    #                         ~entrez_search(db="sra", term=.x)$count)
+    dna_seq = map_dbl(dna, 
+                      ~entrez_search(db="pubmed", term=.x)$count),
+    rna_seq = map_dbl(rna, 
+                      ~entrez_search(db="pubmed", term=.x)$count)
     )
 
 
@@ -41,9 +37,26 @@ human_genome_search %>%
   pivot_longer(-anio, 
                values_to = "valores", 
                names_to = "names") %>% 
+  mutate(names=factor(names,
+                      levels = c("dna_seq", "rna_seq"),
+                      labels = c("DNA-seq", "RNA-seq"))) %>% 
   ggplot(aes(anio, valores, fill=names, color=names)) +
   geom_bar(stat="identity",size=1, position = position_dodge()) +
-  scale_fill_manual(values = c("pink", "skyblue","green", "yellow")) +
-  scale_color_manual(values = c("red", "blue","forestgreen", "orange")) +
-  theme_test() +
-  theme(legend.position = "top")
+  scale_fill_manual(values = c("pink", "skyblue")) +
+  scale_color_manual(values = c("red", "blue")) +
+  scale_y_continuous(expand = expansion(0),
+                     limits = c(0,30000),
+                     breaks = seq(0,30000,5000)) +
+  scale_x_continuous(expand = expansion(0),
+                     limits = c(1950, 2022),
+                     breaks = seq(1949,2022, 10)) + 
+  labs(
+    title = "Publicaciones DNA-seq vs RNA-seq, PUBMED",
+    x="Año",
+    y="Número de publicaciones",
+    fill = NULL,
+    color=NULL
+  ) +
+  theme_classic() +
+  theme(legend.position = "top",
+        plot.title = element_text(hjust = .5, size = 18))
