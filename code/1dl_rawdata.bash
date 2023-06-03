025b6941821f82a2bbcf5eeefccfece0c4d83d93
JuanCarlos <jcge97@gmail.com>
2023-06-03 23:23:57 +0100

    Añadir 1dl_rawdata a las reglas, cambiamos .gitignore

--- code/1dl_rawdata.bash
+++ code/1dl_rawdata.bash
@@ -3,29 +3,43 @@
 ## ftp from the files bams -> add the path of the report.tsv of the project 
 ## when execute the script
 
+#################
+## DIRECTORIES ##
+#################
+
+if [[ ! -d data/original_bam/ ]]
+then
+    mkdir data/original_bam/
+fi
+
+#################
+##  VARIABLES  ##
+#################
+
 ## Extracting to a variable the bam files from report.tsv
-bam_files=$(cat $1 \
-    | cut -f8 \
+bam_files=$(cat metadata/report.tsv \
+    | cut -f 8 \
     | grep -v "submitted_ftp")
 
 ## We transform the bam files list into an array
 bam_array=$(echo $bam_files)
 
-read -p "Ruta donde quiere las lecturas crudas: " rawdata
-
+#################
+##  EXECUTION  ##
+#################
 
 ## This loop will dowload the bam files from the project to the
 ## "original_bam" directory
 for bam in ${bam_array[*]}
 do
-    if [ ! -f ${rawdata}$(echo $bam \
+    if [ ! -f data/original_bam/$(echo $bam \
         | cut -d "/" -f6) ]
     then
-        wget -P $rawdata $bam
-    elif [ -f ${rawdata}$(echo $bam \
+        wget -P data/original_bam/ $bam
+    elif [ -f data/original_bam/$(echo $bam \
         | cut -d "/" -f6) ]
     then
-        echo "===>>> THE FILE ${rawdata}$(echo $bam \
+        echo "===>>> THE FILE data/original_bam/$(echo $bam \
         | cut -d "/" -f6) ALREADY EXIST!!! <<<==="
     else
         echo "==>>> ERROR <<<=="
