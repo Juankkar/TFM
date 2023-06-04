79c2311bcacb93d0192753509f2ba4d09510d7fe
JuanCarlos <jcge97@gmail.com>
2023-06-04 21:31:30 +0100

    Prácticamente terminado, Anotación de Variantes en CLI hecha :)

---
+++ code/6vep.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#################
+## DIRECTORIES ##
+#################
+
+if [[ ! -d results/variants/vep/ ]]
+then
+    mkdir results/variants/vep/
+fi
+
+#################
+##  VARIABLES  ##
+#################
+
+read -p "Select your sample VCF file for VEP -> " sample
+read -p "Select assembly (example: GRCh38) -> " assembly
+read -p "Select your species (example: homo_sapiens) -> " specie 
+
+#################
+##  EXECUTION  ##
+#################
+
+vep -i results/variants/${sample}.vcf \
+    --cache \
+    --assembly ${assembly} \
+    --offline \
+    --species ${specie} \
+    --force_overwrite \
+    -o results/variants/vep/${sample}.txt \
+    --vcf \
+    --fork 4 \
+    --plugin dbNSFP
