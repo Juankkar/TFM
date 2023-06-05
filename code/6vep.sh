fec0b6a4e726c92fa6582fe29db9f69a17c1b30e
JuanCarlos <jcge97@gmail.com>
2023-06-05 18:23:49 +0100

    Añadiendo parámetros a 6vep.sh
    Former-commit-id: 1996a55e740df180495a9d6ddc41bd7f8ea9a1c3

--- code/6vep.sh
+++ code/6vep.sh
@@ -21,13 +21,40 @@ read -p "Select your species (example: homo_sapiens) -> " specie
 ##  EXECUTION  ##
 #################
 
+# vep -i results/variants/${sample}.vcf \
+#     --cache \
+#     --assembly ${assembly} \
+#     --offline \
+#     --species ${specie} \
+#     --force_overwrite \
+#     -o results/variants/vep/${sample}.txt \
+#     --vcf \
+#     --fork 4
+
+## No se si me va a dar tiempo de subir esto antes de que lo veas, pero creo que asi esta mas completo :)
+## Supongo que sobraran cosas 
 vep -i results/variants/${sample}.vcf \
-    --cache \
-    --assembly ${assembly} \
     --offline \
-    --species ${specie} \
     --force_overwrite \
-    -o results/variants/vep/${sample}.txt \
-    --vcf \
-    --fork 4
-    
+    --assembly ${assembly} \
+    --af \
+    --af_1kg \
+    --appris \
+    --biotype \
+    --buffer_size 500 \
+    --check_existing \
+    --distance 5000 \
+    --filter_common \
+    --mane \
+    --polyphen b \
+    --protein \
+    --pubmed \
+    --regulatory \
+    --sift b \
+    --species $specie \
+    --symbol \
+    --transcript_version \
+    --tsl \
+    --uniprot \
+    --cache \
+    -o results/variants/vep/${sample}.txt
