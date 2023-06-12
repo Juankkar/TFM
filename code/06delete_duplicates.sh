d8501574f0c2e8173f74c13a69bc2c989d6b1722
JuanCarlos <jcge97@gmail.com>
2023-06-12 16:29:25 +0100

    Principalmente outputs (touched flags)

--- code/06delete_duplicates.sh
+++ code/06delete_duplicates.sh
@@ -24,7 +24,7 @@ do
     ## Mark duplicates
     picard MarkDuplicates --INPUT results/mapped_reads/bam_files/${sample}_sorted.bam \
         --OUTPUT results/mapped_reads/bam_files/${sample}_dedup.bam \
-        --METRICS_FILE results/mapped_reads/bam_files/markduplicates/markDuplicatesMetrics.txt \
+        --METRICS_FILE results/mapped_reads/bam_files/markduplicates/${sample}_markDuplicatesMetrics.txt \
         --ASSUME_SORTED True
 
     ## Indexamos de nuevo el bam de duplicados en este caso
