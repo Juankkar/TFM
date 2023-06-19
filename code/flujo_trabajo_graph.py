dab62f9721f28213a5beec6db2773d8e29bf0338
Juan Carlos García Estupiñán <104734299+Juankkar@users.noreply.github.com>
2023-06-19 15:13:44 +0100

    Add files via upload
    Añadir un gráfico del flujo de trabajo
    
    Former-commit-id: 42d20d4e52fd5a2093fe60cee9422f568829c61b

---
+++ code/flujo_trabajo_graph.py
@@ -0,0 +1,43 @@
+import pygraphviz as pgv
+
+# Definir las reglas del grafo
+graph_content = """
+digraph snakemake_rules {{
+    rankdir=LR;
+    size="15,6";
+    label="Rule 1 to Rule 15"
+    
+    // Node styling
+    node [style=filled];
+    "rule download_data" [fillcolor=orange];
+    "rule pre_processing" [fillcolor=green];
+    "rule reference_genome" [fillcolor=green];
+    "rule fastqc" [fillcolor=green];
+    "rule fastp" [fillcolor=green];
+    "rule fastqc_trimmed" [fillcolor=green];
+    "rule bwa_mapping" [fillcolor=green];
+    "rule merge_sam_files" [fillcolor=green];
+    "rule sam_to_bam" [fillcolor=green];
+    "rule delete_duplicates" [fillcolor=green];
+    "rule extracting_variants" [fillcolor=green];
+    "rule vep_install_db" [fillcolor=orange];
+    "rule vep_cli" [fillcolor=green];
+    "rule parsing_dataR" [fillcolor=green];
+    "rule R_plotting" [fillcolor=orange];
+    
+    // Edge connections
+    
+    "1. Preparación del flujo" -> "rule download_data" -> "rule pre_processing"  -> "rule reference_genome"; 
+    "2. Vizualizar y arreglar" -> "rule fastqc" -> "rule fastp" -> "rule fastqc_trimmed" -> "3. Mapeado, SAM/BAM" -> "rule bwa_mapping" -> "rule merge_sam_files" -> "rule sam_to_bam" -> "rule delete_duplicates"; 
+    "4. Estudio de variantes" -> "rule extracting_variants" -> "rule vep_install_db" -> "rule vep_cli" -> "rule parsing_dataR" -> "rule R_plotting";
+}}
+"""
+
+# Crear un objeto de gráfico
+graph = pgv.AGraph(string=graph_content)
+
+# Establecer las opciones de diseño
+graph.layout(prog='dot')
+
+# Guardar el gráfico como una imagen
+graph.draw('snakemake_rules_linear.png')
