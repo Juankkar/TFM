0189f2ccb8c2b9e5d66e413c7726ac1bedcfe835
JuanCarlos <jcge97@gmail.com>
2023-08-02 16:20:25 +0100

    Mejorando el script de python para cambiar los nombres

--- code/02rename.py
+++ code/02rename.py
@@ -3,39 +3,27 @@
 #################
 ##  LIBRERIES  ##
 #################
-
+import pandas as pd
 import os
 
 ################
-##  VARIBLES  ##old_filename_list
+##  VARIBLES  ##
 ################
 
-old_filename_list = ["data/original_bam/C2466_GCCAAT.bwa.sorted.rmdup.recal.realigned.fixed.bam",
-                     "data/original_bam/C2467_CGATGT.bwa.sorted.rmdup.recal.realigned.fixed.bam",
-                     "data/original_bam/C2468_TTAGGC.bwa.sorted.rmdup.recal.realigned.fixed.bam",
-                     "data/original_bam/C3481_GCCAAT.bwa.sorted.rmdup.recal.realigned.fixed.bam",
-                     "data/original_bam/C3852_CTTGTA.bwa.sorted.rmdup.recal.realigned.fixed.bam",
-                     "data/original_bam/C3890_TGACCA.bwa.sorted.rmdup.recal.realigned.fixed.bam",
-                     "data/original_bam/NG-5502_C2463.bwa.sorted.rmdup.recal.realigned.sorted.fixed.bam",
-                     "data/original_bam/NG-5502_C2461.bwa.sorted.rmdup.recal.realigned.sorted.fixed.bam",
-                     "data/original_bam/C-3861_GCCAAT_L007.bwa.sorted.rmdup.recal.realigned.fixed.bam",
-                     "data/original_bam/C2213_CGATGT.bwa.sorted.rmdup.recal.realigned.fixed.bam",
-                     "data/original_bam/C896_TGACCA.bwa.sorted.rmdup.recal.realigned.fixed.bam",
-                     "data/original_bam/C3988_ACAGTG.bwa.sorted.rmdup.recal.realigned.fixed.bam"]
+## We will use some of the fields of report.tsv in metadata
+report = pd.read_csv("metadata/report.tsv", sep="\t")
 
-new_filename_list = ["data/original_bam/ERR696683.bam",
-                     "data/original_bam/ERR753368.bam",
-                     "data/original_bam/ERR753369.bam",
-                     "data/original_bam/ERR753370.bam",
-                     "data/original_bam/ERR753371.bam",
-                     "data/original_bam/ERR753372.bam",
-                     "data/original_bam/ERR753373.bam",
-                     "data/original_bam/ERR753374.bam",
-                     "data/original_bam/ERR753375.bam",
-                     "data/original_bam/ERR753376.bam",
-                     "data/original_bam/ERR753377.bam",
-                     "data/original_bam/ERR753378.bam"]
+submitted_ftp_list = list(report.submitted_ftp)
+old_filename_list = []
+for name in range(len(submitted_ftp_list)):
+    names = "data/original_bam/" + submitted_ftp_list[name].split("/")[-1]
+    old_filename_list.append(names)
 
+run_accession_list = list(report.run_accession)
+new_filename_list = []
+for name in range(len(run_accession_list)):
+    names = "data/original_bam/" + run_accession_list[name] + ".bam"
+    new_filename_list.append(names)
 
 #################
 ##  EXECUTION  ##
