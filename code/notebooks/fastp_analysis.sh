626ab4713b118e9836a095f461ab37bdf2598dce
JuanCarlos <jcge97@gmail.com>
2023-08-04 15:38:14 +0100

    fastp analysis

---
+++ code/notebooks/fastp_analysis.sh
@@ -0,0 +1,15 @@
+#!usr/bin/env bash
+
+echo -e "nseqs_before_fastp_1\tnseqs_after_fastp_1\tnseqs_before_fastp_2\tnseqs_after_fastp_2\tsample" \
+    > fastp_analysis.tsv
+
+for sample in ERR696683 ERR7533{68..78}
+do
+    before_fastp_1=$(zcat ../../data/raw/${sample}*_1* | grep @ -c)
+    after_fastp_1=$(zcat ../../data/processed/${sample}*_1* | grep @ -c)
+
+    before_fastp_2=$(zcat ../../data/raw/${sample}*_2* | grep @ -c)
+    after_fastp_2=$(zcat ../../data/processed/${sample}*_2* | grep @ -c)
+
+    echo -e "$before_fastp_1\t$after_fastp_1\t$before_fastp_2\t$after_fastp_2\t$sample"
+done >> fastp_analysis.tsv
