8f63a5f80dcd1c6445ff9e600547736d4e0ed25d
JuanCarlos <jcge97@gmail.com>
2023-08-08 12:59:37 +0100

    crear funciones para el programa de vcf stats para resumir las operaciones

--- code/analysis/scripts_analysis/07vcfstats.sh
+++ code/analysis/scripts_analysis/07vcfstats.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env bash
 
+function vcf_basicstats_filter(){
+
+  operation=$()$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
+    | sed 's/:\s/\t/g' \
+    | grep -E $1 \
+    | cut -f 2 \
+    | awk '{sum += $1} END {print sum}')
+
+      echo $operation
+}
+
+function vcf_stats_filter() {
+
+  operation=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
+    | sed 's/:\s/\t/g' \
+    | grep -E $1 \
+    | cut -f 2 \
+    | sed 's/)//' \
+    | sed 's/\s(/\t/' \
+    | sed 's/\//\t/' \
+    | awk '{sum_col2 += $2; sum_col3 += $3} END {print sum_col2"|"sum_col3}')
+
+    echo $operation
+}
+
+
 path=../../../metadata/logs/vcfstats/ 
 
 echo -e "pass_fail_filter\tsnps\tmnps\tinsertions\tdeletions\tindels\tsame_reference\tsample" \
@@ -8,61 +34,21 @@ echo -e "pass_fail_filter\tsnps\tmnps\tinsertions\tdeletions\tindels\tsame_refer
 for sample in ERR696683 ERR7533{68..78}
 do
 
-  passed_filters=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Pass" \
-    | cut -f 2 \
-    | awk '{sum += $1} END {print sum}')
+  passed_filters=$(vcf_basicstats_filter "^Pass")
 
-  failed_filters=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Fail" \
-    | cut -f 2 \
-    | awk '{sum += $1} END {print sum}')
+  failed_filters=$(vcf_basicstats_filter "^Fail") 
 
-  snps=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^SNPs" \
-    | cut -f 2 \
-    | awk '{sum += $1} END {print sum}')
+  snps=$(vcf_basicstats_filter "^SNPs") 
 
+  mnps=$(vcf_basicstats_filter "^MNPs") 
 
-  mnps=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^MNPs" \
-    | cut -f 2 \
-    | awk '{sum += $1} END {print sum}')
+  inser=$(vcf_basicstats_filter "^Insertions") 
 
-  inser=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Insertions" \
-    | cut -f 2 \
-    | awk '{sum += $1} END {print sum}')
+  del=$(vcf_basicstats_filter "^Deletions")  
 
+  indel=$(vcf_basicstats_filter "^Indels") 
 
-  inser=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Insertions" \
-    | cut -f 2 \
-    | awk '{sum += $1} END {print sum}')
-
-  del=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Deletions" \
-    | cut -f 2 \
-    | awk '{sum += $1} END {print sum}')
-
-  indel=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Indels" \
-    | cut -f 2 \
-    | awk '{sum += $1} END {print sum}')
-
-  same_ref=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Same" \
-    | cut -f 2 \
-    | awk '{sum += $1} END {print sum}')
+  same_ref=$(vcf_basicstats_filter "^Same") 
 
   echo -e "$passed_filters|$failed_filters\t$snps\t$mnps\t$inser\t$del\t$indel\t$same_ref\t$sample"
 
@@ -73,96 +59,23 @@ echo -e "snp_transition_transversion\ttotal_het_hom\tsnp_het_hom\tmnp_het_hom\tI
 for sample in ERR696683 ERR7533{68..78} 
 do
 
-  snp_trans=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^SNP\sT" \
-    | cut -f 2 \
-    | sed 's/)//' \
-    | sed 's/\s(/\t/' \
-    | sed 's/\//\t/' \
-    | awk '{sum_col2 += $2; sum_col3 += $3} END {print sum_col2"|"sum_col3}')
+  snp_trans=$(vcf_stats_filter "^SNP\sT")
 
-  tot_het_hom=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Total\sH" \
-    | cut -f 2 \
-    | sed 's/)//' \
-    | sed 's/\s(/\t/' \
-    | sed 's/\//\t/' \
-    | awk '{sum_col2 += $2; sum_col3 += $3} END {print sum_col2"|"sum_col3}')
+  tot_het_hom=$(vcf_stats_filter "^Total\sH")
 
-  tot_het_hom=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Total\sH" \
-    | cut -f 2 \
-    | sed 's/)//' \
-    | sed 's/\s(/\t/' \
-    | sed 's/\//\t/' \
-    | awk '{sum_col2 += $2; sum_col3 += $3} END {print sum_col2"|"sum_col3}')
+  snp_het_hom=$(vcf_stats_filter "^SNP\sH") 
 
-  snp_het_hom=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^SNP\sH" \
-    | cut -f 2 \
-    | sed 's/)//' \
-    | sed 's/\s(/\t/' \
-    | sed 's/\//\t/' \
-    | awk '{sum_col2 += $2; sum_col3 += $3} END {print sum_col2"|"sum_col3}')
+  mnp_het_hom=$(vcf_stats_filter "^MNP\sH") 
 
-  mnp_het_hom=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^MNP\sH" \
-    | cut -f 2 \
-    | sed 's/)//' \
-    | sed 's/\s(/\t/' \
-    | sed 's/\//\t/' \
-    | awk '{sum_col2 += $2; sum_col3 += $3} END {print sum_col2"|"sum_col3}')
+  insert_het_hom=$(vcf_stats_filter "^Insertion\sH")
 
-  insert_het_hom=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Insertion\sH" \
-    | cut -f 2 \
-    | sed 's/)//' \
-    | sed 's/\s(/\t/' \
-    | sed 's/\//\t/' \
-    | awk '{sum_col2 += $2; sum_col3 += $3} END {print sum_col2"|"sum_col3}')
+  del_het_hom=$(vcf_stats_filter "^Deletion\sH")  
 
-  del_het_hom=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Deletion\sH" \
-    | cut -f 2 \
-    | sed 's/)//' \
-    | sed 's/\s(/\t/' \
-    | sed 's/\//\t/' \
-    | awk '{sum_col2 += $2; sum_col3 += $3} END {print sum_col2"|"sum_col3}')
+  indel_het_hom=$(vcf_stats_filter "^Indel\sH")
 
-  indel_het_hom=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Indel\sH" \
-    | cut -f 2 \
-    | sed 's/)//' \
-    | sed 's/\s(/\t/' \
-    | sed 's/\//\t/' \
-    | awk '{sum_col2 += $2; sum_col3 += $3} END {print sum_col2"|"sum_col3}')
+  insert_del=$(vcf_stats_filter "^Insertion\/Deletion")
 
-  insert_del=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Insertion\/Deletion" \
-    | cut -f 2 \
-    | sed 's/)//' \
-    | sed 's/\s(/\t/' \
-    | sed 's/\//\t/' \
-    | awk '{sum_col2 += $2; sum_col3 += $3} END {print sum_col2"|"sum_col3}') 
-
-  indel_snp_plus_mnp=$(sed -E 's/\s:\s/\t/g' ${path}${sample}* \
-    | sed 's/:\s/\t/g' \
-    | grep -E "^Indel\/SNP" \
-    | cut -f 2 \
-    | sed 's/)//' \
-    | sed 's/\s(/\t/' \
-    | sed 's/\//\t/' \
-    | awk '{sum_col2 += $2; sum_col3 += $3} END {print sum_col2"|"sum_col3}') 
-
+  indel_snp_plus_mnp=$(vcf_stats_filter "^Indel\/SNP")
 
   echo -e "$snp_trans\t$tot_het_hom\t$snp_het_hom\t$mnp_het_hom\t$insert_het_hom\t$del_het_hom\t$indel_het_hom\t$insert_del\t$indel_snp_plus_mnp\t$sample"
 
